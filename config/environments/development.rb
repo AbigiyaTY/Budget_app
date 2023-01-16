103b47f25fd1fd50b12673410772c65201736784
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-16 19:30:49 +0300

    Add url link to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://budget-app-ybru.onrender.com', port: 3000 }
 
   config.action_mailer.delivery_method = :letter_opener
 
